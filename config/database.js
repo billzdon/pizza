97e84115fa1323795f213bd2bbbdffcb50c13b61
Bill Zdon <williamzdon@gmail.com>
2018-08-31 21:56:37 +0200

    turn off ssl for now

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ var pool = new Pool({
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   port: 5432,
-  ssl: true,
+  ssl: false,
   max: 20,
   min: 4,
   idleTimeoutMillis: 1000,
